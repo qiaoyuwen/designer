d68a2573080f80eadcbed006d77a6b005e55d0f2
ys <704807396@qq.com>
2022-12-11 13:31:48 +0800

    生产域名修改

--- packages/manager/src/configs/app.pre.ts
+++ packages/manager/src/configs/app.pre.ts
@@ -3,7 +3,7 @@ import { IAppConfig } from '@/models'
 const config: Partial<IAppConfig> = {
   appKey: 'dsy_zbpp5J4mDC1T',
   endpointCode: 'end_R2vypKMnc613',
-  foundByteBigdataURL: 'http://bigdata-screen-v3-dev.gogdev.cn/',
+  foundByteBigdataURL: 'https://fastblend.onebuygz.com/',
 }
 
 export default config
